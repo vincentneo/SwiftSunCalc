de01ce4c19fe1ddc004387f1fde0cd4391e44957
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2020-07-26 14:31:14 +0800

    Initial Commit

---
+++ SwiftSunCalc/SwiftSunCalc.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSunCalc.h
+//  SwiftSunCalc
+//
+//  Created by Vincent Neo on 26/7/20.
+//  Copyright © 2020 Vincent Neo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftSunCalc.
+FOUNDATION_EXPORT double SwiftSunCalcVersionNumber;
+
+//! Project version string for SwiftSunCalc.
+FOUNDATION_EXPORT const unsigned char SwiftSunCalcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSunCalc/PublicHeader.h>
+
+
